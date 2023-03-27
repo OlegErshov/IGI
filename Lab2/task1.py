ceeba82d20c257f391361157ecd80bcfda1f9fd8
OlegErshov <olezhka1629@gmail.com>
2023-03-27 15:03:58 +0300

    add n-grams search

--- Lab2/task1.py
+++ Lab2/task1.py
@@ -1,6 +1,6 @@
 import re
 import os
-
+from collections import Counter
 
 def print_text():
     """Prints the text in the file"""
@@ -123,3 +123,27 @@ def get_average_length_of_words_str(a)->int:
         words_count += 1
     return int((length - no_word_length) * 100 / (words_count - no_words_count)) / 100
 
+def get_top_K_repeated_N_grams(text, k=10, n=4):
+    sentences = get_amount_of_sentences_from_string(text)
+
+    patern = r"(\w+|\d+)"
+    match = re.findall(patern,text)
+
+    not_word_pattern = r"(?<=\s)(\d+)(?=\s)"
+    no_word_match = re.findall(not_word_pattern,text)
+    
+    all_words = match - no_word_match
+    
+    words_by_sentence = [all_words.findall(
+        sent) for sent in sentences]
+
+    n_grams = []
+    for words in words_by_sentence:
+        n_grams.extend(list(zip(*[words[i:] for i in range(n)])))
+
+    counter = Counter()
+
+    for n_gram in n_grams:
+        counter[' '.join(n_gram)] += 1
+
+    return dict(counter.most_common(k))
