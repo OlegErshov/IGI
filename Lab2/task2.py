6aa3a6fd181f53355ff89438575c7fd51624a399
OlegErshov <olezhka1629@gmail.com>
2023-03-27 14:17:58 +0300

    add container for task 2

---
+++ Lab2/task2.py
@@ -0,0 +1,55 @@
+import re
+import os
+import ujson
+
+class User:
+    def init(self):
+        self.elements: set = set()
+        self.username: str = str(input("Enter username: ")) 
+        answer = str(input("Load data (Yes / No)?\n")) 
+        if answer == "Yes": 
+            self.load()
+    
+    def load(self):
+        try: 
+            file_stream = open( 
+                os.path.join("data", self.username + ".json"), "r", encoding="utf8" 
+            ) 
+            self.elements = self.elements.union(set(ujson.load(file_stream))) 
+        except FileNotFoundError: 
+            pass 
+
+    def save(self):
+        with open(os.path.join("data", self.username + ".json"), "w", encoding="utf8") as fp: 
+            ujson.dump(list(self.elements), fp) 
+    
+    def add(self,obj):
+        self.elements.add(obj)
+
+    def remove(self,obj):
+        try: 
+            self.elements.remove(obj) 
+        except KeyError: 
+            pass 
+
+    def exists(self,objects: list):
+        found_any = False 
+        for obj in objects: 
+            if len(obj) == 0: 
+                continue 
+            if set(obj).issubset(self.elements): 
+                print(f"{obj} - exists") 
+                found_any = True 
+        if not found_any: 
+            print("No such elements") 
+
+    def grep(self,expression: str) -> None:
+        found_any = False 
+        for value in self.elements: 
+            if len(value) == 0: 
+                continue 
+            if re.fullmatch(expression, str(value)) is not None: 
+                print(f"{value} - matches the expression") 
+                found_any = True 
+        if not found_any: 
+            print("No such elements")
