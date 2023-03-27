660e6687cda7cac15ca70f581c28e7f88215f4a0
OlegErshov <olezhka1629@gmail.com>
2023-03-27 13:43:36 +0300

    add function to calculate average of sentences and words

--- Lab2/tack1.py
+++ Lab2/tack1.py
@@ -27,3 +27,45 @@ def get_amount_of_nondec_sentences_str(a) -> int:
     pattern = r"(?<![A-Z\.])(?<!Mr)(\!+|\?+|\!\?|\?\!|\;)(?=\s[A-Z])"
     match = re.findall(pattern, text)
     return len(match)
+
+def get_average_length_of_sentences() -> int:
+    f = open(os.path.join(os.getcwd(), "text.txt"), "r", encoding="utf-8")
+
+    text = f.read()
+    patern = r"(\w+|\d+)"
+    match = re.findall(patern,text)
+
+    not_word_pattern = r"(?<=\s)(\d+)(?=\s)"
+    no_word_match = re.findall(not_word_pattern,text)
+
+    no_word_length = 0
+    for val in no_word_match:
+        no_word_length += len(val)
+    length = 0
+    for val in match:
+        length += len(val)
+    return (length - no_word_length)/get_amount_of_sentences()
+
+
+def get_average_length_of_words()->int:
+    f = open(os.path.join(os.getcwd(), "text.txt"), "r", encoding="utf-8")
+
+    text = f.read()
+    patern = r"(\w+|\d+)"
+    match = re.findall(patern,text)
+    not_word_pattern = r"(?<=\s)(\d+)(?=\s)"
+    no_word_match = re.findall(not_word_pattern,text)
+
+    no_word_length = 0
+    no_words_count = 0
+    for val in no_word_match:
+        no_word_length += len(val)
+        no_words_count += 1
+    
+    length = 0
+    words_count = 0
+    for val in match:
+        length += len(val)
+        words_count += 1
+    return (length - no_word_length) / (words_count - no_words_count)
+
