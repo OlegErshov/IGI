ee73fa974891ef3ccf078843797b95b19fca8325
OlegErshov <olezhka1629@gmail.com>
2023-05-08 10:23:14 +0300

    update files system and add first test

---
+++ Lab3/Tests/constants.py
@@ -0,0 +1,8 @@
+
+
+test_list = [53, "I want 10 pls", True, (66.4, "pleeease")]
+test_tuple = (53, "I want 10 pls", True, 66.4)
+test_bytes = b":)"
+test_set = {53, "I want 10 pls", True, 66.4}
+
+test_dict = {test_tuple: {89: "dog", "cat": False}, "Butoma": 91.79}
